b5d99c99ab81a9e28da5662130e4c0ee771dbcfc
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2026-06-19 09:03:11 +0200

    chore(ci): make build process more generic

--- default.nix
+++ default.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/bin
-    cp huburl $out/bin
-    chmod +x $out/bin/huburl
+    cp ${manifest.name} $out/bin
+    chmod +x $out/bin/${manifest.name}
   '';
 }
